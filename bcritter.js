c89f8f7bec0798976b3e5955b32779989a0a1822
Paolo Pironi <ppironi@loretto.com>
2017-11-23 12:21:18 +0000

    added some code

--- bcritter.js
+++ bcritter.js
@@ -1,15 +1,62 @@
+const vector = require('./vector').vector;
+const utils = require('./world');
+
 const BouncingCritter = () => 
 {
     return {
         direction: randomElement(directionNames),
         act(view) {
-
+            if (view.look(direction) != ' ') {
+                direction = view.find(' ') || 's';
+            }
+            return {
+                type: 'move',
+                direction: direction
+            }
         }
     }
 }
 
 const directionNames = "n ne e se s sw w nw".split(" ");
+const directions = {
+    "n":  vector( 0, -1),
+    "ne": vector( 1, -1),
+    "e":  vector( 1,  0),
+    "se": vector( 1,  1),
+    "s":  vector( 0,  1),
+    "sw": vector(-1,  1),
+    "w":  vector(-1,  0),
+    "nw": vector(-1, -1)
+  };
 const randomElement = (array) =>
 {
     return array[Math.floor(Math.random() * array.length)];
+}
+
+const View = (world, vector) => {
+    return {
+        look(direction) {
+            const target = vector.plus(directions[direction]);
+            if (world.grid.isInside(target)) {
+                return utils.charFromElement(world.grid.get(target));
+            }
+            return '#'
+        },
+        findAll(ch) {
+            let found = [];
+            for (dir in directions) {
+                if (this.look(dir) == ch) {
+                    found.push(dir);
+                }
+            }
+            return found;
+        },
+        find(ch) {
+            let found = this.findAll(ch);
+            if (found.length == 0) {
+                return null;
+            }
+            return randomElement(found);
+        }
+    }
 }
--- world.js
+++ world.js
@@ -1,9 +1,27 @@
 const vector = require('./vector').vector;
+const elementFromChar = (legend, ch) => {
+    if (ch == ' ')
+        return null;
+    const element = legend[ch]();
+    element.originChar = ch;
+    return element;
+}
+
+const charFromElement = (element) => {
+    if (element == null) {
+        return ' ';
+    } else {        
+        return element.originChar;
+    }
+}
 
 const Grid = (width, height) =>
 {
     const space = Array(width * height);
     return {
+        space: space,
+        width: width,
+        height: height,
         isInside(vector) {
             return (
                 vector.x >= 0 && vector.x < width &&
@@ -19,10 +37,9 @@ const Grid = (width, height) =>
     }
 }
 
-/* const grid = Grid(10,10);
-console.log(grid.isInside(vector(5,5)));
-grid.set(vector(5,5), 6);
-console.log(grid.get(vector(5,5))); */
+/* const grid = Grid(2,2);
+grid.set(vector(1,1), 6);
+console.log(grid.space); */
 
 const World = (map, legend) => 
 {
@@ -35,22 +52,38 @@ const World = (map, legend) =>
     })
 
     return {
+        grid: grid,
         toString() {
-
+            let output = '';
+            for (let y = 0; y < grid.height; y++) {
+                for (let x = 0; x < grid.width; x++) {
+                    let element = grid.get(vector(x, y));
+                    output += charFromElement(element);
+                }
+                output += '\n';
+            }
+            return output;
         }
     }
 }
 
-const elementFromChar = (legend, ch) => {
-    if (ch = ' ')
-        return null;
-    const element = legend[ch]();
-    element.originChar = ch;
-    return element;
-}
 
-const charFromElement = (element) => {
-    if (element == null)
-        return ' ';
-    return element.originChar;
-}
+const plan = ["############################",
+            "#      #    #      o      ##",
+            "#                          #",
+            "#          #####           #",
+            "##         #   #    ##     #",
+            "###           ##     #     #",
+            "#           ###      #     #",
+            "#   ####                   #",
+            "#   ##       o             #",
+            "# o  #         o       ### #",
+            "#    #                     #",
+            "############################"];
+
+w1 = World(plan, {
+    '#': '#',
+    'o': 'o'
+});
+
+console.log(w1.toString());
